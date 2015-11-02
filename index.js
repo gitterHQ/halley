ca592e16f770f62bb5243f703d8cddde7ab784bf
Andrew Newdigate <andrew@troupe.co>
2015-11-02 16:03:17 +0000

    Fixed node entry-point

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ require('./lib/util/externals').use({
 });
 
 var Faye = { };
-var Transport = require('./transport/transport');
-Faye.Client = require('./protocol/client');
+var Transport = require('./lib/transport/transport');
+Faye.Client = require('./lib/protocol/client');
 
-Transport.register('websocket'   , require('./transport/node/node-websocket'));
-Transport.register('long-polling', require('./transport/node/node_http'));
+Transport.register('websocket'   , require('./lib/transport/node/node-websocket'));
+Transport.register('long-polling', require('./lib/transport/node/node_http'));
 
 module.exports = Faye;
