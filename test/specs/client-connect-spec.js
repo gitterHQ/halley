bad0c4433310982ec966b1c70d1f673a37a5a4bf
Andrew Newdigate <andrew@troupe.co>
2016-01-12 14:59:28 +0000

    Fixes #2

---
+++ test/specs/client-connect-spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var assert = require('assert');
+var Promise = require('bluebird');
+
+module.exports = function() {
+  describe('client-connect', function() {
+
+    it('should not timeout on empty connect messages', function() {
+      var client = this.client;
+      var connectionWentDown = false;
+      client.on('connection:down', function() {
+        connectionWentDown = true;
+      });
+
+      return client.connect()
+        .then(function() {
+          return Promise.delay(client._advice.timeout + 1000);
+        })
+        .then(function() {
+          assert(!connectionWentDown);
+        })
+
+    });
+
+  });
+
+};
--- test/specs/client-spec.js
+++ test/specs/client-spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function() {
+  require('./client-connect-spec')();
   require('./client-subscribe-spec')();
   require('./client-publish-spec')();
   require('./client-reset-spec')();
