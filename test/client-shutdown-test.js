18e9d39b9232b9b83e0e0a90cc148acd52a83581
Andrew Newdigate <andrew@troupe.co>
2016-01-13 17:15:43 +0000

    Fixed leaky websockets

--- test/client-shutdown-test.js
+++ test/client-shutdown-test.js
@@ -6,6 +6,9 @@ var assert = require('assert');
 describe('client-shutdown-test', function() {
 
   it('should cleanup after disconnect', function(done) {
+    // See https://github.com/petkaantonov/bluebird/issues/926
+    this.timeout(45000);
+
     var testProcess = fork(__dirname + '/helpers/cleanup-test-process', [this.urlDirect]);
 
     testProcess.on('close', function (code) {
